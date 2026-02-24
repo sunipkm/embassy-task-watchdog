ff4aed05e298565491745f26a7bb53a8ed29bdb6
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2026-02-24 03:05:37 -0500

    Clean up all subdirectories

---
+++ scripts/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+current_dir=$(pwd)
+script_dir=$(dirname $(dirname $(readlink -f "$0")))
+
+# puts us back in the current directory on failure
+function cleanup {
+    cd "$current_dir" || exit 1
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# subdirs
+examples="$script_dir/examples/*"
+crates="$script_dir/embassy-task*"
+
+# run cargo fmt on all subdirs
+for dir in $crates $examples; do
+    if [ -d "$dir" ]; then
+        echo "Formatting $dir..."
+        cd "$dir"
+        cargo clean
+    else
+        echo "Skipping $dir, not a directory"
+    fi
+done
